bebbe393aa354cd6078e257aab552663bf2fcc7c
codePen-ctrl <spyae9266@gmail.com>
2026-03-01 22:47:24 +0630

    Set default port to 5000 if not specified

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors({
 
 app.use(express.json({ limit: '50mb' }));
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const secret_key = process.env.SECRET_KEY;
 
 const db_host = process.env.DB_HOST;
